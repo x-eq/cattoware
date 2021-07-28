9bbb3072859f92c4aded2f6a5052c00be3182100
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2021-07-28 15:29:16 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
+local success, returned = pcall(function() 
+    return game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/games/" .. game.PlaceId .. ".lua")
+end)
+if not (success and returned and #returned ~= 0) then 
+    return error("Game not supported!")
+end
 
+print("Game supported! Loading the script.")
+loadstring(tostring(returned))()
