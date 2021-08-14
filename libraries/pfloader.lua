5a378c768932e0c1e35b1c3ff766415ea789f484
Spoorloos <59575072+Spoorloos@users.noreply.github.com>
2021-08-14 21:35:00 +0200

    Update pfloader.lua

--- libraries/pfloader.lua
+++ libraries/pfloader.lua
@@ -1,49 +1,51 @@
 local loader = { }; do
 	table.insert(loader, function()
 		local ui, error = loadstring(game:HttpGet("https://raw.githubusercontent.com/CatzCode/cattoware/main/libraries/ui.lua", true))
-		return table.pack(ui()), (error == nil)
+		return ui(), (error == nil)
 	end)
 	table.insert(loader, function()
 		local success, returned = pcall(function() 
 			local client = { }; do
 				client["physics"] = require(game:GetService("ReplicatedFirst").SharedModules.Old.Utilities.Math.physics:Clone())
 				for i,v in pairs(getgc(true)) do
-					if (type(v) == "function") then
-						local name = debug.getinfo(v).name; do
-							if (string.lower(name) == "getgunlist") then
-								client["getgunlist"] = v
-							elseif (string.lower(name) == "loadplayer") then
-								debug.setupvalue(v, 1, "")
-								client["loadplayer"] = v
-							elseif (string.lower(name) == "loadgun") then
-								client["loadgun"] = v
-							elseif (string.lower(name) == "bulletcheck") then
-								client["bcheck"] = v 
+					spawn(function() 
+						if (type(v) == "function") then
+							local name = debug.getinfo(v).name; do
+								if (string.lower(name) == "getgunlist") then
+									client["getgunlist"] = v
+								elseif (string.lower(name) == "loadplayer") then
+									debug.setupvalue(v, 1, "")
+									client["loadplayer"] = v
+								elseif (string.lower(name) == "loadgun") then
+									client["loadgun"] = v
+								elseif (string.lower(name) == "bulletcheck") then
+									client["bcheck"] = v 
+								end
+							end
+						elseif (type(v) == "table") then
+							if (rawget(v, "step") and rawget(v, "reset") and rawget(v, "new")) then
+								client["particle"] = v
+							elseif (rawget(v, "PlaySoundId")) then
+								client["sound"] = v
+							elseif (rawget(v, "setmovementmode")) then
+								client["character"] = v
+							elseif (rawget(v, "send")) then
+								client["network"] = v
+							elseif (rawget(v, "gammo")) then
+								client["gamelogic"] = v
+							elseif (rawget(v, "basecframe")) then
+								client["camera"] = v
+							elseif (rawget(v, "breakwindow")) then
+								client["effects"] = v
+							elseif (rawget(v, "firehitmarker")) then
+								client["uieffects"] = v
+							elseif (rawget(v, "getbodyparts")) then
+								client["replication"] = v
+							elseif (rawget(v, "bulletLifeTime")) then
+								client["publicsettings"] = v
 							end
 						end
-					elseif (type(v) == "table") then
-						if (rawget(v, "step") and rawget(v, "reset") and rawget(v, "new")) then
-							client["particle"] = v
-						elseif (rawget(v, "PlaySoundId")) then
-							client["sound"] = v
-						elseif (rawget(v, "setmovementmode")) then
-							client["character"] = v
-						elseif (rawget(v, "send")) then
-							client["network"] = v
-						elseif (rawget(v, "gammo")) then
-							client["gamelogic"] = v
-						elseif (rawget(v, "basecframe")) then
-							client["camera"] = v
-						elseif (rawget(v, "breakwindow")) then
-							client["effects"] = v
-						elseif (rawget(v, "firehitmarker")) then
-							client["uieffects"] = v
-						elseif (rawget(v, "getbodyparts")) then
-							client["replication"] = v
-						elseif (rawget(v, "bulletLifeTime")) then
-							client["publicsettings"] = v
-						end
-					end
+					end)
 				end
 			end
 			return client
